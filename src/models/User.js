1dff02fee48972629ea4bfdf10c5512760512f90
João Victor <dacunharosajoaovictor@gmail.com>
2026-05-13 21:47:45 -0300

    Corrigido erro do bcrypt e middleware

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -7,6 +7,7 @@ const UserSchema = new mongoose.Schema({
     required: true,
     trim: true
   },
+
   email: {
     type: String,
     required: true,
@@ -14,29 +15,28 @@ const UserSchema = new mongoose.Schema({
     lowercase: true,
     trim: true
   },
+
   password: {
     type: String,
     required: true,
     minlength: 6
   }
+
 }, {
-  timestamps: true // createdAt, updatedAt
+  timestamps: true
 });
 
-// Criptografa senha antes de salvar
-UserSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
-  
-  try {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (error) {
-    next(error);
+UserSchema.pre('save', async function () {
+
+  if (!this.isModified('password')) {
+    return;
   }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+
 });
 
-// Método para comparar senha
 UserSchema.methods.comparePassword = async function(candidatePassword) {
   return await bcrypt.compare(candidatePassword, this.password);
 };
